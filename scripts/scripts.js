7c4ac5bc7c9adcf6bda3c75ae708cbeec19dfadb
Zoran Nikolovski <nikolovs@adobe.com>
2023-11-14 11:03:42 +1100

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -16,7 +16,7 @@ import {
 const LCP_BLOCKS = []; // add your LCP blocks to the list
 
 /**
- * Builds hero block and prepends to main in a new section.
+ * Builds hero block and prepends to main in a new section. 
  * @param {Element} main The container element
  */
 function buildHeroBlock(main) {
