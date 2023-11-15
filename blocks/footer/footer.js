fd1e1093c86f7595ec6a7f4b6e02769a9aa8915a
Zoran Nikolovski <nikolovs@adobe.com>
2023-11-15 11:47:13 +1100

    Update footer.js

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -1,21 +1,46 @@
-import { decorateIcons, getMetadata } from '../../scripts/lib-franklin.js';
+// import { decorateIcons, getMetadata } from '../../scripts/lib-franklin.js';
+
+// /**
+//  * loads and decorates the footer
+//  * @param {Element} block The footer block element
+//  */
+// export default async function decorate(block) {
+//   // const cfg = readBlockConfig(block);
+//   block.textContent = '';
+
+//   const footerMeta = getMetadata('footer');
+//   const footerPath = footerMeta ? new URL(footerMeta).pathname : '/footer';
+//   const resp = await fetch(`${footerPath}.plain.html`);
+
+//   // fetch footer content
+//   // const footerPath = cfg.footer || '/footer';
+//   // const resp = await fetch(`${footerPath}.plain.html`,
+//   // window.location.pathname.endsWith('/footer') ? { cache: 'reload' } : {});
+
+//   if (resp.ok) {
+//     const html = await resp.text();
+
+//     // decorate footer DOM
+//     const footer = document.createElement('div');
+//     footer.innerHTML = html;
+
+//     decorateIcons(footer);
+//     block.append(footer);
+//   }
+// }
+import { readBlockConfig, decorateIcons } from '../../scripts/lib-franklin.js';
 
 /**
  * loads and decorates the footer
  * @param {Element} block The footer block element
  */
 export default async function decorate(block) {
-  // const cfg = readBlockConfig(block);
+  const cfg = readBlockConfig(block);
   block.textContent = '';
 
-  const footerMeta = getMetadata('footer');
-  const footerPath = footerMeta ? new URL(footerMeta).pathname : '/footer';
-  const resp = await fetch(`${footerPath}.plain.html`);
-
   // fetch footer content
-  // const footerPath = cfg.footer || '/footer';
-  // const resp = await fetch(`${footerPath}.plain.html`,
-  // window.location.pathname.endsWith('/footer') ? { cache: 'reload' } : {});
+  const footerPath = cfg.footer || '/footer';
+  const resp = await fetch(`${footerPath}.plain.html`, window.location.pathname.endsWith('/footer') ? { cache: 'reload' } : {});
 
   if (resp.ok) {
     const html = await resp.text();
